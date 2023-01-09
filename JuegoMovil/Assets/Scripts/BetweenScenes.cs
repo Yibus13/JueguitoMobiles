2f07d82a6c0e37f76855f91d18c05acf7b85f935
Yibus13 <jesusculebras.scf@gmail.com>
2023-01-09 17:53:39 +0100

    Ya estaaa

--- JuegoMovil/Assets/Scripts/BetweenScenes.cs
+++ JuegoMovil/Assets/Scripts/BetweenScenes.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class BetweenScenes : MonoBehaviour
 {
     public static int level = 0;
-    public static bool level1 = false;
-    public static bool level2 = false;
-    public static bool level3 = false;
+    public static bool level1 = true;
+    public static bool level2 = true;
+    public static bool level3 = true;
 }
--- JuegoMovil/Assets/Scripts/Match3GameManager.cs
+++ JuegoMovil/Assets/Scripts/Match3GameManager.cs
@@ -190,6 +190,7 @@ public class Match3GameManager : MonoBehaviour
         DestroyArray.AddRange(Vertical());
 
         if (DestroyArray.Count == 0) MoveTile(x1, y1, x2, y2);//si no hay casillas a eliminar se vuelven a cambiar las posiciones
+        else GameObject.Find("SoundManager").GetComponent<AudioManager>().Play("Destroy");
 
         DestroySquares();
 
@@ -213,6 +214,7 @@ public class Match3GameManager : MonoBehaviour
     }
     void DestroySquares()
     {
+        
         List<Tile> SquaresEliminated = Horizontal();
         SquaresEliminated.AddRange(Vertical());
 
